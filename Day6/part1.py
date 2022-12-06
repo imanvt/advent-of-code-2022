988f2fffa746d95a47b1cd6569061fcafdf13397
Iman Malik <iman8252000@gmail.com>
2022-12-05 21:09:00 -0800

    day 6 solutions

--- Day6/part1.py
+++ Day6/part1.py
@@ -1,5 +1,11 @@
 
-with open('test.txt', 'r') as f:
+with open('input.txt', 'r') as f:
     lines = f.readlines()
     for line in lines:
         line = line.strip('\n')
+        for i in range(len(line)):
+            s = set(line[i:i+4])
+            if len(s) == 4:
+                print(i+4)
+                break
+
